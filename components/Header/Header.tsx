cbe1c00474e16dd8cb05685624bd6637bc187b56
Ralph Largo <devlargs@gmail.com>
2022-05-28 04:41:42 +0800

    feat: responsive header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import { Search2Icon } from '@chakra-ui/icons';
-import { Box, Flex, Input, InputGroup, InputLeftElement, Text } from '@chakra-ui/react';
+import { Box, chakra, Flex, Input, InputGroup, InputLeftElement, Text } from '@chakra-ui/react';
 import { useEffect, useState, VFC } from 'react';
 import { AiFillGithub } from 'react-icons/ai';
 import useRedditSearch from 'store/useRedditInput';
@@ -19,11 +19,25 @@ const Header: VFC = () => {
     <Box h="80px" bg="blue.900" position="fixed" width="100%" px="4" zIndex={1}>
       <Flex h="80px" alignItems="center" maxW="1200px" m="auto" justifyContent="space-between">
         <Flex alignItems="center">
-          <Text ml="4" mr="10" color="white" fontWeight="bold">
+          <Text
+            ml="4"
+            mr="10"
+            color="white"
+            fontWeight="bold"
+            d={{
+              base: 'none',
+              md: 'initial',
+            }}
+          >
             Imgur Subreddit Galleries
           </Text>
 
-          <InputGroup width="200px">
+          <InputGroup
+            width={{
+              base: '100%',
+              md: '200px',
+            }}
+          >
             <InputLeftElement pointerEvents="none" color="white">
               <Search2Icon color="gray.300" />
             </InputLeftElement>
@@ -37,9 +51,17 @@ const Header: VFC = () => {
           </InputGroup>
         </Flex>
 
-        <a href="https://github.com/devlargs/reddit-images-gallery" target="_blank" rel="noreferrer">
+        <chakra.a
+          href="https://github.com/devlargs/reddit-images-gallery"
+          target="_blank"
+          rel="noreferrer"
+          d={{
+            base: 'none',
+            md: 'initial',
+          }}
+        >
           <AiFillGithub color="white" fontSize="20px" cursor="pointer" />
-        </a>
+        </chakra.a>
       </Flex>
     </Box>
   );
