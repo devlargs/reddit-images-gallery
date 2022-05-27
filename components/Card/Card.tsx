d886f82fde0a8cc888339c8ef2e09643c91de47f
Ralph Largo <devlargs@gmail.com>
2022-05-28 04:34:36 +0800

    feat: added simple animation for cards

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,25 +1,44 @@
 import { Box, BoxProps } from '@chakra-ui/react';
-import { FC } from 'react';
+import { motion } from 'framer-motion';
+import { FC, ReactElement } from 'react';
+import { InView } from 'react-intersection-observer';
+
+const transitionVariant = {
+  fadeOut: { opacity: 0, y: -10 },
+  fadeIn: { opacity: 1, y: 0 },
+};
 
 const Card: FC<BoxProps> = ({ children, ...restProps }) => {
   return (
-    <Box
-      bg="white"
-      border="1px solid lightgray"
-      mb="4"
-      borderRadius="3px"
-      p={{
-        base: 5,
-        md: '10',
-      }}
-      _hover={{
-        border: '1px solid black',
-      }}
-      transition="0.5s ease-in"
-      {...restProps}
-    >
-      {children}
-    </Box>
+    <InView triggerOnce threshold={0.2}>
+      {({ inView, ref }): ReactElement => (
+        <motion.div
+          ref={ref}
+          variants={transitionVariant}
+          initial="fadeOut"
+          animate={inView ? 'fadeIn' : 'fadeOut'}
+          transition={{ ease: 'easeInOut', duration: 1.2 }}
+        >
+          <Box
+            bg="white"
+            border="1px solid lightgray"
+            mb="4"
+            borderRadius="3px"
+            p={{
+              base: 5,
+              md: '10',
+            }}
+            _hover={{
+              border: '1px solid black',
+            }}
+            transition="0.5s ease-in"
+            {...restProps}
+          >
+            {children}
+          </Box>
+        </motion.div>
+      )}
+    </InView>
   );
 };
 
